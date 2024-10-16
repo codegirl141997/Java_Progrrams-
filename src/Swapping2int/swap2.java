2367e44fa4627cd3bf653f4803e9556573e930b4
codegirl1411997 <dhanashreee14@gmail.com>
2024-10-16 18:24:36 +0530

    Swapping of of Two Integer

---
+++ src/Swapping2int/swap2.java
@@ -0,0 +1,26 @@
+package Swapping2int;
+
+public class swap2 {
+
+	public static void main(String[] args) {
+		
+		int a =10;
+		int b =20;
+		
+		int c;
+		
+		
+		 c =a ; //10
+		 
+		 a= b; //20
+		 
+		 b= c;
+		 
+		 System.out.println(a);
+		 System.out.println(b);
+		
+		
+
+	}
+
+}
